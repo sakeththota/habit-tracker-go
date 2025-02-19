35357e649c3f3cd1d7786f34f7cfe76e45927f3d
sakeththota <sakeththota01@gmail.com>
2025-02-19 17:54:11 -0600

    validating ownership when getting habit progress

--- service/progress/routes.go
+++ service/progress/routes.go
@@ -22,26 +22,13 @@ func NewHandler(store types.ProgressStore, userStore types.UserStore, habitStore
 
 func (h *Handler) RegisterRoutes(router *gin.RouterGroup) {
 	router.GET("/progress/hello", h.handleGetProgressHello)
-	router.GET("/progress/:id", auth.WithJWTAuth(h.handleGetProgress, h.userStore))
+	router.GET("/progress/:id", auth.ValidateOwnership(auth.WithJWTAuth(h.handleGetProgress, h.userStore), h.habitStore))
 }
 
 func (h *Handler) handleGetProgress(c *gin.Context) {
-	userID := auth.GetUserIDFromContext(c)
 	habitID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Errorf("invalid habit id: %v", err)})
-		return
-	}
-
-	// should this be its own validation function in the habits service?
-	// h.habitStore.ValidateOwnership(habitID, userID)
-	habit, err := h.habitStore.GetHabitById(habitID)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Errorf("failed to get habit by id: %v", err)})
-		return
-	}
-	if habit.UserID != userID {
-		c.JSON(http.StatusForbidden, gin.H{"error": fmt.Errorf("permission denied, failed to get progress of habit with id: %v", err)})
 		return
 	}
 
